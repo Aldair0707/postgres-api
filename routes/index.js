f9553e43a6c1ade6fbe99c2b005b89909e21d1dc
Aldair0707 <Aldair0707>
2025-04-19 01:09:27 -0600

    avance

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ router.get('/', (req, res) => res.send('Welcome'));
 
 router.post('/users', controllers.createUser);
 router.get('/users', controllers.getAllUsers);
+router.delete('/users/:id', controllers.deleteUser);
+router.put('/users/:id', controllers.updateUser);
+router.get('/users/:id', controllers.getUser);
 
 
 module.exports = router;
